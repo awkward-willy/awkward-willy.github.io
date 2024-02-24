1d402d6be25b807d4aaf89a66d410670ddd8e5ee
Willy_Awkward <willy1118t@gmail.com>
2024-02-24 22:44:14 +0800

    [Feat] Update EducationSection component and install react-vertical-timeline-component.

--- app/about/_components/EducationSection.tsx
+++ app/about/_components/EducationSection.tsx
@@ -1,5 +1,54 @@
+"use client";
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import Constants from "../_constants";
+import Image from "next/image";
+
 const EducationSection = () => {
-  return <div>EducationSection</div>;
+  return (
+    <>
+      <h1 className="font-extrabold text-2xl sm:text-3xl">Education</h1>
+      <div className="m-3">
+        <VerticalTimeline lineColor="#9DA6B7" animate={true}>
+          {Constants.education.map((data, index) => {
+            return (
+              <VerticalTimelineElement
+                key={index}
+                visible={true}
+                className="vertical-timeline-element--work"
+                contentStyle={{ background: "#C1CADA", color: "#000" }}
+                contentArrowStyle={{
+                  borderRight: "7px solid #C1CADA",
+                }}
+                date={data.time}
+                iconStyle={{
+                  background: "#C1CADA",
+                  color: "#000",
+                }}
+                textClassName="text-black"
+                icon={
+                  <Image src={data.icon} alt="icon" width={50} height={50} />
+                }
+                iconClassName="flex items-center justify-center"
+              >
+                <h1 className="vertical-timeline-element-title text-xl font-medium">
+                  {data.school}
+                </h1>
+                <h3 className="vertical-timeline-element-subtitle">
+                  {data.department}
+                </h3>
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
 };
 
 export default EducationSection;
--- app/about/_constants/index.ts
+++ app/about/_constants/index.ts
@@ -3,6 +3,21 @@ const Constants = {
     Language: ["Traditional Chinese (Native)", "English (Fluent)"],
     Nationality: ["Taipei, Taiwan"],
   },
+  education: [
+    {
+      school: "National Central University(NCU)",
+      department: "Computer Science and Information Engineering",
+      time: "2021-Present",
+      icon: "/NCULogo.png",
+    },
+    {
+      school:
+        "The Affiliated Senior High School of National Taiwan Normal University (HSNU)",
+      department: "High School (Graduated)",
+      time: "2019-2021",
+      icon: "/HSNULogo.png",
+    },
+  ],
 };
 
 export default Constants;
