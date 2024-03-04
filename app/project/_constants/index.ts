ce2b8c322482847fb4e707dbaeea9c2df99f1068
Willy_Awkward <willy1118t@gmail.com>
2024-03-05 01:11:44 +0800

    [Add] Finish project page.

---
+++ app/project/_constants/index.ts
@@ -0,0 +1,60 @@
+const Color = {
+  React: "bg-[#61DAFB]",
+  "Next.js": "bg-white",
+  "Tailwind CSS": "bg-[#38BDF8]",
+  "Three.js": "bg-gray-200",
+  SCSS: "bg-[#CF649A]",
+  HTML: "bg-red-400",
+  JavaScript: "bg-yellow-400",
+  Python: "bg-[#38BDF8]",
+  Flutter: "bg-[#54C5F8]",
+};
+
+const ProjectConstants = {
+  Web: [
+    {
+      Title: "Portfolio (This Website)",
+      Feature: ["The website you are currently viewing!"],
+      Image: "/projects/Portfolio.png",
+      Link: "/",
+      Tag: ["React", "Next.js", "Tailwind CSS", "Shadcn UI", "Three.js"],
+    },
+    {
+      Title: "Former GitHub Page",
+      Feature: ["First Web Page!"],
+      Image: "/projects/FormerGitHubPage.png",
+      Link: "https://awkward-willy.github.io/FormerGitHubPage/",
+      Tag: ["HTML", "SCSS", "JavaScript"],
+    },
+  ],
+  Others: [
+    {
+      Title: "Wordle",
+      Feature: ["Simple Wordle Clone", "Tkinter GUI"],
+      Image: "/projects/Wordle.png",
+      Link: "https://github.com/awkward-willy/Wordle",
+      Tag: ["Python"],
+    },
+  ],
+  NCU_Course: [
+    {
+      Title: "NCU Reservation App",
+      Feature: [
+        "A course project to build a reservation App for study room in NCU.",
+        "Pure UI design only.",
+      ],
+      Image: "/projects/NCU_Reservation_App.png",
+      Link: "https://github.com/awkward-willy/NCU_Reservation_app_personal",
+      Tag: ["Flutter"],
+    },
+    {
+      Title: "MiniGomoku",
+      Feature: ["Socket Programming", "PyQt", "Pygame"],
+      Image: "/projects/MiniGomoku.png",
+      Link: "https://github.com/awkward-willy/MiniGomoku",
+      Tag: ["Python"],
+    },
+  ],
+};
+
+export { Color, ProjectConstants };
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,5 +1,9 @@
 import { Metadata } from "next";
-
+import { Color, ProjectConstants } from "./_constants";
+import Image from "next/image";
+import { DotIcon, DoubleArrowRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 export const metadata: Metadata = {
   title: "Project",
 };
@@ -9,6 +13,77 @@ const ProjectPage = () => {
     <>
       <div className="flex flex-col m-2 gap-3">
         <h1 className="font-extrabold text-2xl sm:text-3xl">Projects</h1>
+        {Object.keys(ProjectConstants).map((key, index) => {
+          return (
+            <div key={index} className="flex flex-col gap-3 my-4">
+              <h2 className="font-bold text-lg sm:text-xl">{key}</h2>
+              <div className="flex flex-wrap gap-3">
+                {ProjectConstants[key as keyof typeof ProjectConstants].map(
+                  (data, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="flex flex-col gap-2 max-w-80 sm:w-96 border-2 p-4 rounded-md h-min shadow-md shadow-border"
+                      >
+                        <div className="overflow-hidden flex justify-center">
+                          <Image
+                            src={`${data.Image}`}
+                            alt={data.Title}
+                            width={300}
+                            height={200}
+                            className="w-auto h-auto max-h-80 object-contain border-2 rounded-md"
+                          />
+                        </div>
+                        <h3 className="font-bold text-base">{data.Title}</h3>
+                        {
+                          <>
+                            {data.Feature.map((feature, index) => {
+                              return (
+                                <div
+                                  key={index}
+                                  className="flex gap-2 items-center"
+                                >
+                                  <DoubleArrowRightIcon className="w-4 h-4 flex-shrink-0" />
+                                  <span className="text-sm">{feature}</span>
+                                </div>
+                              );
+                            })}
+                          </>
+                        }
+                        <Link
+                          href={data.Link}
+                          target="_blank"
+                          className={cn(
+                            "flex flex-col gap-2 underline text-sm",
+                            data.Link === "/" ? "hidden" : "visible"
+                          )}
+                        >
+                          Link to Project
+                        </Link>
+                        <div className="flex flex-wrap gap-1.5">
+                          {data.Tag.map((tag, index) => {
+                            return (
+                              <div
+                                key={index}
+                                className={`flex items-center gap-0.5 text-xs sm:text-sm text-[hsl(184,83%,7%)] px-2 py-1 rounded-md ${
+                                  Color[tag as keyof typeof Color] ||
+                                  "bg-gray-300"
+                                }`}
+                              >
+                                <DotIcon className="w-3 h-3" />
+                                {tag}
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
