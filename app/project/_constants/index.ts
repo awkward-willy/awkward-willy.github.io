72c2ed2db558974763c25b477ce31e37b6156b7d
Willy_Awkward <willy1118t@gmail.com>
2024-03-08 22:37:45 +0800

    [Fix] Change link to fix 404 error caused by same-origin.

--- app/project/_constants/index.ts
+++ app/project/_constants/index.ts
@@ -23,7 +23,7 @@ const ProjectConstants = {
       Title: "Former GitHub Page",
       Feature: ["First Web Page!"],
       Image: "/projects/FormerGitHubPage.png",
-      Link: "/FormerGitHubPage",
+      Link: "https://github.com/awkward-willy/FormerGitHubPage",
       Tag: ["HTML", "SCSS", "JavaScript"],
     },
   ],
